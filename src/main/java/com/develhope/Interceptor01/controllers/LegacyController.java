3377a6b979ebdd3cb2a583e2258817fa801a4718
Domenico Ribaudo <domenicoribaudo06@gmail.com>
2024-03-25 22:18:38 +0100

    Initial commit

---
+++ src/main/java/com/develhope/Interceptor01/controllers/LegacyController.java
@@ -0,0 +1,20 @@
+package com.develhope.Interceptor01.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/legacy")
+public class LegacyController {
+    /**
+ * This method is responsible for returning a legacy message.
+ *
+ * @return the legacy message
+ */
+@GetMapping("/message")
+public String getLegacy() {
+    return "This is just old code";
+}
+
+}
